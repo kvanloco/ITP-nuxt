bbbe0adc04f461e8e7f622214def88b2ddb6f6d1
kvanloco <kvanloco@gmail.com>
2022-12-08 20:18:39 +0000

    add ListRow

---
+++ components/LIstRow.vue
@@ -0,0 +1,27 @@
+<template>
+    <div data-pg-name="list-item" class="
+            border-b
+            flex flex-auto flex-row
+            hover:bg-gray-200 hover:border-b-2
+          ">
+        <p class="flex flex-shrink-0 font-light p-1 text-black w-16" data-pg-name="id">
+            #1235
+        </p>
+        <p class="flex font-bold p-1 text-blue-600 w-28" data-pg-name="type">
+            Site Activity
+        </p>
+        <p class="flex flex-auto flex-grow p-1" data-pg-name="title">
+            SAT PR01
+        </p>
+        <p class="flex p-1 text-yellow-500" data-pg-name="status">Open</p>
+        <p class="flex p-1 text-black" data-pg-name="by">Comete</p>
+        <p class="flex p-1" data-pg-name="due">10/11/2022</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
