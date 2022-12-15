be5d68e6826136628e40492597f5337e98237da1
kvanloco <kvanloco@gmail.com>
2022-12-15 10:23:37 +0100

    update

---
+++ components/Sidenav.vue
@@ -0,0 +1,19 @@
+<script>
+const menuItems = [
+    {
+        name: "item 1",
+        to: ""
+    }
+]
+</script>
+
+<template>
+      <div class="flex top-0 h-screen flex-col flex-wrap p-2 w-56 border-r">
+        <h6 class="not-italic py-3">Enter your heading</h6>
+        <ul class="list-disc list-inside">
+          <li class="flex items-center gap-x-3 xl:">List item 1</li>
+          <li class="flex items-center gap-x-3">List item 2</li>
+          <li class="flex items-center gap-x-3">List item 3</li>
+        </ul>
+      </div>
+</template>
