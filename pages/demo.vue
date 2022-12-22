4b0221c782f84893724626a3a5e5073d410bab82
kvanloco <kvanloco@gmail.com>
2022-12-22 16:52:22 +0100

    2 files modified, 2 added

--- pages/demo.vue
+++ pages/demo.vue
@@ -88,7 +88,9 @@ const onClickCopy = (value) => {
           <div v-else class="justify-center flex-grow flex px-2 gap-2 py-5">
             <div v-for="(field, index) in namingFields" class="flex-auto">
               <!-- Template -->
-              <NamingField v-bind="field" :index="index" @output="onOutput" />
+              <client-only placeholder="Loading...">
+                <NamingField v-bind="field" :index="index" @output="onOutput" />
+              </client-only>
             </div>
           </div>
 
