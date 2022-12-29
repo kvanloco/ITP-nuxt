9acc8d3372ec9e90d276dd8bc169214dafbb7630
kvanloco <kvanloco@gmail.com>
2022-12-29 10:14:22 +0100

    4 files modified

--- pages/demo.vue
+++ pages/demo.vue
@@ -86,7 +86,7 @@ const onClickCopy = (value) => {
     <div class="h-screen flex flex-row flex-wrap text-sm">
       <div class="flex-1 p-6">
         <div class="">
-          <div class="pb-3 border-top">
+          <div class="pb-3">
             <h6 class="font-semibold text-xl">your template name</h6>
             <p class="">Lorem ipsum dolar sit</p>
 
