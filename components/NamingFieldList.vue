dd6a28fe0d29ef535a99203825b8e927eadbfccc
kvanloco <kvanloco@gmail.com>
2023-01-05 16:26:41 +0100

    1 file added

---
+++ components/NamingFieldList.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="justify-center flex-grow flex px-2 gap-2 py-5">
+    <div v-for="(field, index) in namingFields" class="flex-auto">
+      <!-- Template  Naming fields-->
+      <client-only placeholder="Loading...">
+        <NamingField v-bind="field" :index="index" @output="onOutput" />
+      </client-only>
+    </div>
+  </div>
+</template>
