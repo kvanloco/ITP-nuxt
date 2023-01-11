b2437b4052c453e3300adf8f718c2d1ed75160f9
kvanloco <kvanloco@gmail.com>
2023-01-11 15:51:18 +0100

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,4 +18,19 @@ export default defineNuxtConfig({
     supabase_url: process.env.SUPABASE_URL,
     supabase_api_key: process.env.SUPABASE_KEY,
   },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Document Naming Tool',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        {
+          name: 'description',
+          content:
+            'Create structured document and file names and get yourself and your team on the same page!',
+        },
+      ],
+    },
+  },
 });
